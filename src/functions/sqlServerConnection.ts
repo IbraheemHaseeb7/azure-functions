e6ffa9e84e281330484b4260e002ecba21d8c0a9
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2024-03-13 17:20:59 +0500

    [FIXED]: now sql connection is made via input bindings which manages connection pooling on its own

--- src/functions/sqlServerConnection.ts
+++ src/functions/sqlServerConnection.ts
@@ -3,35 +3,31 @@ import {
     HttpRequest,
     HttpResponseInit,
     InvocationContext,
+    input,
 } from "@azure/functions";
-import { connect } from "mssql";
+
+const sql = input.sql({
+    commandText: "SELECT * FROM dbo.Employees",
+    commandType: "Text",
+    connectionStringSetting: "SqlConnectionString",
+});
 
 export async function sqlServerConnection(
     request: HttpRequest,
     context: InvocationContext
 ): Promise<HttpResponseInit> {
     try {
-        const connection = await connect({
-            database: process.env["Database"],
-            user: process.env["User"],
-            password: process.env["Password"],
-            server: process.env["Server"],
-            options: {
-                encrypt: true,
-                trustServerCertificate: false,
-            },
-        });
+        const result = context.extraInputs.get(sql);
 
-        const result = await connection.query`SELECT * FROM Employees`;
-
-        return { status: 200, jsonBody: { data: result.recordset } };
+        return { status: 200, jsonBody: { data: result } };
     } catch (e: Error | any) {
         return { status: 500, jsonBody: { message: e.message } };
     }
 }
 
 app.http("sqlServerConnection", {
-    methods: ["GET", "POST"],
+    methods: ["GET"],
     authLevel: "anonymous",
+    extraInputs: [sql],
     handler: sqlServerConnection,
 });
